74bf1bc6beeed4e28b6e5fcb01bab4d794449615
Tru <tru.zhou@gmail.com>
2022-07-01 03:41:29 +0800

    hotfix for click spaces revert space part from commit e6a17ceadf1f4531a5704297b7c533e4a8a0df96

--- index.jsx
+++ index.jsx
@@ -90,6 +90,10 @@ const render = ({ output, error }) => {
 
   const displayId = parseInt(window.location.pathname.replace('/', ''))
   const displayIndex = displays.find((d) => d.id === displayId).index
+  const visibleSpaces = spaces.reduce((acc, space) => {
+    const { 'is-visible': isVisible, visible: __legacyIsVisible, index } = space
+    return isVisible ?? __legacyIsVisible ? [...acc, index] : acc
+  }, [])
 
   const classes = Utils.classnames(baseClasses, { 'simple-bar--no-shadow': shadow !== 'on' })
 
